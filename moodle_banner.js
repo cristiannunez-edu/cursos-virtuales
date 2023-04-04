ba38439ae4cdf56794401c7908fa1d76904f9cc9
Cristian Nuñez <cristiannunez@alumno.utesa.edu>
2023-04-04 09:58:17 -0400

    Agregar Orientaciones Generales

--- moodle_banner.js
+++ moodle_banner.js
@@ -20,6 +20,9 @@
           <h3>¡Sean bienvenidos/as!</h3>
           <p>El curso inicia el <b class="course-date"></b></p>
         </div>
+        <div style="width: 100%;">
+          <div style="position: relative; padding-bottom: 56.25%; padding-top: 0; height: 0;"><iframe title="Orientaciones Generales - INFOTEP" frameborder="0" width="1200" height="675" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" src="https://view.genial.ly/637319db776f50001a1d1c82" type="text/html" allowscriptaccess="always" allowfullscreen="true" scrolling="yes" allownetworking="all"></iframe> </div>
+        </div
         `
       +
       `
